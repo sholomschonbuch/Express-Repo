b6192d0fab3872ae42d205116c87c5cd8760a623
Simon <sholomschonbuch@gmail.com>
2023-09-12 18:06:29 -0400

    expressJS with advanced routing, middleware...

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const router = express.Router()
+
+router.use(logger)
+
+router.get('/', (req, res) => {
+    console.log(req.query.name)
+    res.send(users)
+})
+
+router.get('/new', (req, res) => {
+    res.render("users/new", { firstName: "Test" })
+})
+
+router.post('/', (req, res) => {
+    const isValid = true
+    if (isValid) {
+        users.push({ name: req.body.firstName })
+        res.redirect(`/users/${users.length - 1}`)
+    } else {
+        console.log("Error")
+        res.render(`users/new`, { firstName: req.body.firstName })
+    }
+    console.log(req.body.firstName)
+    res.send("Hi")
+})
+
+router.route("/:id")
+.get((req, res) => {
+    console.log(req.user)
+    res.send(`ID ${users[req.params.id].name}`)
+})
+.put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+const users = [{ name: "Kyle" }, { name: "Sally" }]
+
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
+module.exports = router
